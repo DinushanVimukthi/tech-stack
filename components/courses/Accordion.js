d61fcc7ac3ca31d9321eb912b43b73334d0ab5f1
Dinushan Vimukthi <stdinushan@gmail.com>
2023-01-22 14:55:32 +0530

    Added Basic Structure

---
+++ components/courses/Accordion.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import AccaStyles from "@/styles/Acca.module.css";
+import Image from "next/image";
+import ExpandBtn from "../../public/icons/navexpand.png";
+
+export default function Accordion(props) {
+    const [isShowing, setIsShowing] = useState(false);
+
+    const toggle = () => {
+        setIsShowing(!isShowing);
+    };
+
+    return (
+        <div className={AccaStyles.accordionItem}>
+            <div className={AccaStyles.accordionItemHeader}>
+                <div className={AccaStyles.accordionItemHeaderTitle} onClick={toggle}>
+                    {props.title}
+                </div>
+                <div className={AccaStyles.expbtn} onClick={toggle}>
+                    <Image src={ExpandBtn} alt="" style={{
+                        transform: isShowing ? "rotate(0deg)" : "rotate(180deg)"
+                    }}></Image>
+                </div>
+            </div>
+            <div className={AccaStyles.accordionItemContent} style={{
+                display: isShowing ? "flex" : "none"
+            }}>
+                {/* eslint-disable-next-line react/no-unescaped-entities */}
+                {props.content}
+            </div>
+        </div>
+    );
+}
